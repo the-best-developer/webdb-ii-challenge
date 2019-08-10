8fc7dbaf20d58b0991e3990624833c4d743c79d3
the-best-developer <im.dylan.gosnell@gmail.com>
2019-08-10 12:46:38 -0400

    Added seed file of existing cars

--- data/migrations/20190809202125_create-cars.js
+++ data/migrations/20190809202125_create-cars.js
@@ -2,7 +2,7 @@
 exports.up = (knex) => {
     return knex.schema.createTable('cars', (table) => {
       table.increments();
-      table.varchar('VIN', 10).unique();
+      table.varchar('VIN', 17).unique();
       table.varchar('Make', 50).notNullable();
       table.varchar('Model', 50).notNullable();
       table.integer('Mileage').notNullable();
